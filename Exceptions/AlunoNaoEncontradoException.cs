236a493ca83cc21e819f755dec552175a645cb46
jacksonoqueiroz <40044470+jacksonoqueiroz@users.noreply.github.com>
2019-12-26 19:06:56 -0300

    Add files via upload

---
+++ Exceptions/AlunoNaoEncontradoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaControleOnLine.BLL.Exceptions
+{
+    public class AlunoNaoEncontradoException : Exception
+    {
+        public AlunoNaoEncontradoException():base()
+        {
+
+        }
+    }
+}
---
+++ Exceptions/FormacaoNaoEncontradoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaControleOnLine.BLL.Exceptions
+{
+    public class FormacaoNaoEncontradoException : Exception
+    {
+        public FormacaoNaoEncontradoException():base()
+        {
+
+        }
+    }
+}
