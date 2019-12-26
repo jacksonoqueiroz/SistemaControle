7eeb59bc794798800a2aae0caf763d0d3247c743
jacksonoqueiroz <40044470+jacksonoqueiroz@users.noreply.github.com>
2019-12-26 19:05:44 -0300

    Add files via upload

---
+++ Autenticacao/LoginBo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaControleOnLine.BLL.Exceptions;
+using SistemaControleOnLine.DAL;
+using SistemaControleOnLine.Entities;
+
+namespace SistemaControleOnLine.BLL.Autenticacao
+{
+    public class LoginBo
+    {
+        private UsuarioDao _usuarioDao;
+
+        public Usuario ObterUsuarioParaLogar(string nomeusuario, string senha)
+        {
+            _usuarioDao = new UsuarioDao();
+
+            var usuario =_usuarioDao.ObterUsuarioPeloUsuarioESenha(nomeusuario, senha);
+            
+            if (usuario == null)
+            {
+                throw new UsuarioNaoCadastradoException() ;
+            }
+
+            else
+            {
+                return usuario;      
+            }
+
+        }
+    }
+}
